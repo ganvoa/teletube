bf8b9f5fb5f1dd831926197312887058c20ebc7e
ngamboa <ngamboa@insacom.cl>
2020-03-27 13:14:30 -0300

    refactor main project

--- frontend/src/icons/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import playIcon from '../assets/img/play.svg'
-import Icon from '@ant-design/icons';
-
-export const PlayIcon = props => 
-        <Icon component={playIcon} />
